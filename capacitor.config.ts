b56ae697a64c8e9594473630a8316968bb962815
mamadou <mamadoudiop735@gmail.com>
2022-06-09 09:53:40 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ticketing-app',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
